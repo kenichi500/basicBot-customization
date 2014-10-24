9a1b77b3a25142bb5ca2e97847a2948fffb1ba2e
kenichi500 <kenichi500@gmail.com>
2014-10-24 14:37:16 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -640,7 +640,7 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "KeniBot :kappa:",
+        botName: "KeniBot :forsenkappa:",
         language: "english",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         maximumAfk: 5000,
